e4d21151549378d1c96815286a34c183d7f44059
Niall Lyons <niall.lyons4@mail.dcu.ie>
2017-01-23 13:33:14 +0000

    Create everywhere.cpp

---
+++ everywhere.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <set>
+#include <string>
+
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    for(int i = 0; i < t; ++i){
+        int n;
+        cin >> n;
+        set<string> s;
+        for(int j = 0; j < n; ++j){
+            string str;
+            cin >> str;
+            s.insert(str);
+        }
+        cout << s.size() << endl;
+    }
+    return 0;
+}
