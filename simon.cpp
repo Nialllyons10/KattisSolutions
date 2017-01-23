81b86829db7a0eb4bd30775ef42f64663c1afd3b
Niall Lyons <niall.lyons4@mail.dcu.ie>
2017-01-23 14:19:12 +0000

    Create simon.cpp

---
+++ simon.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    string s;
+    getline(cin, s); //flush cin
+    while(t--){
+        getline(cin, s);
+        if(s.size() > 10 && s.substr(0,10).compare("simon says") == 0){
+            cout << s.substr(11, string::npos);
+        }
+        cout << endl;
+    }
+}
