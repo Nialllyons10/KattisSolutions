686d0ccc97eddd21ad1965b4d01c21fd8b9103a4
Niall Lyons <niall.lyons4@mail.dcu.ie>
2017-01-23 13:31:38 +0000

    Create areal.cpp

---
+++ areal.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    long long n;
+    cin >> n;
+
+    cout << setprecision(20) << 4*sqrt(n) << endl;
+
+    return 0;
+}
