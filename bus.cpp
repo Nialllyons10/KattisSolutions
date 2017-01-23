aabb42818572fb03f8de94be12dcd0a1e2270769
Niall Lyons <niall.lyons4@mail.dcu.ie>
2017-01-23 13:34:01 +0000

    Create bus.cpp

---
+++ bus.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main(){
+    int n, t;
+    cin >> n;
+    for(int i = 0; i < n; ++i){
+        cin >> t;
+        cout << static_cast<unsigned long>(pow (2.0, t)-1) << endl;
+    }
+    return 0;
+}
